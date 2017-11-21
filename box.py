5a6f817fd33ecd6d35506046fdaf52bb103c8c4b
Johan Hidding <johannes.hidding@gmail.com>
2017-11-21 14:12:03 +0100

    put the filtering functions on the Box object for easier access, move testing code to separate notebook

--- box.py
+++ box.py
@@ -2,6 +2,9 @@ from units import unit
 import numpy as np
 from copy import copy
 
+from filters import (
+    gaussian_filter_2d, gaussian_filter_3d,
+    sobel_filter_2d, sobel_filter_3d)
 
 R_earth = 6.371e3 * unit.km
 day = 0.0027378507871321013 * unit.year
@@ -14,12 +17,33 @@ def is_linear(a, eps=1e-3):
 
 class Box:
     """Stores properties of the coordinate system used."""
-    def __init__(self, nc):
-        self.lat = nc.variables['lat'][:]
-        self.lon = nc.variables['lon'][:]
-        self.time = nc.variables['time'][:]
+    def __init__(self, time, lat, lon):
+        self.time = time
+        self.lat = lat
+        self.lon = lon
+
+    @staticmethod
+    def from_netcdf(nc):
+        """Obtain latitude, longitude and time axes from a given NetCDF
+        object."""
+        lat = nc.variables['lat'][:]
+        lon = nc.variables['lon'][:]
+        time = nc.variables['time'][:]
+        return Box(time, lat, lon)
+
+    @staticmethod
+    def generate(n_lon):
+        """Generate a box without time axis, with the given number of pixels in
+        the longitudinal direction. Latitudes are given half that size to
+        create a 2:1 rectangular projection."""
+        n_lat = n_lon // 2
+        lat = np.linspace(-90.0, 90, n_lat + 1)[1:-1]
+        lon = np.linspace(0., 360., n_lon, endpoint=False)
+        time = None
+        return Box(time, lat, lon)
 
     def __getitem__(self, s):
+        """Slices the box in the same way as you would slice data."""
         new_box = copy(self)
 
         if not isinstance(s, tuple):
@@ -31,12 +55,58 @@ class Box:
         return new_box
 
     @property
+    def shape(self):
+        if self.time is None:
+            return (self.lat.size, self.lon.size)
+        else:
+            return (self.time.size, self.lat.size, self.lon.size)
+
+    @property
     def rectangular(self):
+        """Check wether the given latitudes and longitudes are linear, hence
+        if the data is in geographic projection."""
         return is_linear(self.lat) and is_linear(self.lon)
 
     @property
     def resolution(self):
+        """Gives the resolution of the box in units of years and km.
+        The resolution of longitude is given as measured on the equator."""
         res_lat = np.diff(self.lat[1:-1]).mean() * (np.pi / 180) * R_earth
         res_lon = np.diff(self.lon[1:-1]).mean() * (np.pi / 180) * R_earth
-        res_time = np.diff(self.time[1:-1]).mean() * day
-        return res_time, res_lat, res_lon
+
+        if self.time is not None:
+            res_time = np.diff(self.time[1:-1]).mean() * day
+            return res_time, res_lat, res_lon
+        else:
+            return res_lat, res_lon
+
+    def gaussian_filter(self, data, sigma):
+        """Filters a data set with a Gaussian, correcting for the distortion
+        from the geographic projection.
+
+        :param box: instance of :py:class:`Box`.
+        :param data: data set, dimensions should match ``box.shape``.
+        :param sigma: list of sigmas with the correct dimension.
+        :return: :py:class:`numpy.ndarray` with the same shape as input.
+        """
+        if self.time is not None:
+            return gaussian_filter_3d(self, data, *sigma)
+        else:
+            return gaussian_filter_2d(self, data, *sigma)
+
+    def sobel_filter(self, data, weight=None):
+        """Sobel filter. Effectively computes a derivative.  This filter is
+        normalised to return a rate of change per pixel, or if weights are
+        given, the value is multiplied by the weight to obtain a unitless
+        quantity of change over the given weight.
+
+        :param box: :py:class:`Box` instance
+        :param data: input data, :py:class:`numpy.ndarray` with same shape
+            as ``box.shape``.
+        :param weight: weight of each dimension in combining components into
+            a vector magnitude; should have units corresponding those given
+            by ``box.resolution``."""
+        if self.time is not None:
+            return sobel_filter_3d(self, data, weight)
+        else:
+            return sobel_filter_2d(self, data, weight)
---
+++ filters.py
@@ -0,0 +1,129 @@
+import numpy as np
+from scipy.ndimage import (gaussian_filter, sobel)
+
+
+def gaussian_filter_2d(box, data, sigma_lat, sigma_lon):
+    """Filters a 2D (lat x lon) data set with a Gaussian, correcting for
+    the distortion from the geographic projection.
+
+    :param box: instance of :py:class:`Box`.
+    :param data: data set, dimensions should match ``box.shape``.
+    :param sigma_lat: sigma lat in dimension of distance (e.g. km).
+    :param sigma_lon: sigma lon in dimension of distance (e.g. km).
+    :return: :py:class:`numpy.ndarray` with the same shape as input.
+    """
+    res_lat, res_lon = box.resolution
+    s_lat = (sigma_lat / res_lat).m_as('')
+    s_lon = (sigma_lat / res_lat).m_as('')
+
+    outp = np.zeros_like(data)
+
+    for i, lat_rad in enumerate(box.lat / 180 * np.pi):
+        gaussian_filter(
+            data[i, :],
+            min(data.shape[1], s_lon / np.cos(lat_rad)),
+            mode=['wrap'],
+            output=outp[i, :])
+
+    gaussian_filter(
+        outp, [s_lat, 0.0], mode=['constant', 'wrap'], output=outp, cval=0)
+
+    return outp
+
+
+def gaussian_filter_3d(box, data, sigma_t, sigma_lat, sigma_lon):
+    """Filters a 3D (time x lat x lon) data set with a Gaussian, correcting for
+    the distortion from the geographic projection.
+
+    :param box: instance of :py:class:`Box`.
+    :param data: data set, dimensions should match ``box.shape``.
+    :param sigma_t: sigma time in dimension of time (e.g. year).
+    :param sigma_lat: sigma lat in dimension of distance (e.g. km).
+    :param sigma_lon: sigma lon in dimension of distance (e.g. km).
+    :return: :py:class:`numpy.ndarray` with the same shape as input.
+    """
+
+    res_t, res_lat, res_lon = box.resolution
+    s_t = (sigma_t / res_t).m_as('')
+    s_lat = (sigma_lat / res_lat).m_as('')
+    s_lon = (sigma_lat / res_lat).m_as('')
+
+    outp = np.zeros_like(data)
+    for i, lat_rad in enumerate(box.lat / 180 * np.pi):
+        gaussian_filter(
+            data[:, i, :],
+            min(data.shape[2], s_lon / np.cos(lat_rad)),
+            mode=['reflect', 'wrap'],
+            output=outp[:, i, :])
+
+    gaussian_filter(
+        outp, [s_t, s_lat, 0.0], mode=['reflect', 'reflect', 'wrap'],
+        output=outp)
+
+    return outp
+
+
+def sobel_filter_2d(box, data, weight=None, physical=True):
+    """Sobel filter in 2D (lat x lon). Effectively computes a derivative.
+    This filter is normalised to return a rate of change per pixel, or
+    if weights are given, the value is multiplied by the weight to obtain
+    a unitless quantity of change over the given weight.
+
+    :param box: :py:class:`Box` instance
+    :param data: input data, :py:class:`numpy.ndarray` with same shape
+        as ``box.shape``.
+    :param weight: weight of each dimension in combining components into
+        a vector magnitude; should have units corresponding those given
+        by ``box.resolution``.
+    :param physical: wether to correct for geometric projection, by dividing
+        the derivative in the longitudinal direction by the cosine of the
+        latitude."""
+    if weight is None:
+        y = [1/8, 1/8]
+    else:
+        y = [(1/8 * w / r).m_as('') for w, r in zip(weight, box.resolution)]
+
+    sb = np.array([
+        sobel(data, mode=['reflect', 'wrap'], axis=i) * y[i]
+        for i in range(2)])
+
+    if physical:
+        sb[1, :, :] /= np.cos(box.lat / 180 * np.pi)[:, None]
+
+    sb = np.r_[sb, np.ones_like(sb[0:1])]
+    norm = np.sqrt((sb[:-1]**2).sum(axis=0))
+    sb /= norm
+    return sb
+
+
+def sobel_filter_3d(box, data, weight=None, physical=True):
+    """Sobel filter in 3D (time x lat x lon). Effectively computes a
+    derivative.  This filter is normalised to return a rate of change per
+    pixel, or if weights are given, the value is multiplied by the weight to
+    obtain a unitless quantity of change over the given weight.
+
+    :param box: :py:class:`Box` instance
+    :param data: input data, :py:class:`numpy.ndarray` with same shape
+        as ``box.shape``.
+    :param weight: weight of each dimension in combining components into
+        a vector magnitude; should have units corresponding those given
+        by ``box.resolution``.
+    :param physical: wether to correct for geometric projection, by dividing
+        the derivative in the longitudinal direction by the cosine of the
+        latitude."""
+    if weight is None:
+        y = [1/16, 1/16, 1/16]
+    else:
+        y = [(1/16 * w / r).m_as('') for w, r in zip(weight, box.resolution)]
+
+    sb = np.array([
+        sobel(data, mode=['reflect', 'reflect', 'wrap'], axis=i) * y[i]
+        for i in range(3)])
+
+    if physical:
+        sb[2, :, :, :] /= np.cos(box.lat / 180 * np.pi)[None, :, None]
+
+    sb = np.r_[sb, np.ones_like(sb[0:1])]
+    norm = np.sqrt((sb[:-1]**2).sum(axis=0))
+    sb /= norm
+    return sb
