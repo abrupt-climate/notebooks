dafd2e96d27539c6f75271fe79752eeacbe91c91
Johan Hidding <johannes.hidding@gmail.com>
2017-11-30 16:54:42 +0100

    better merging of data sets

--- box.py
+++ box.py
@@ -1,13 +1,28 @@
 from units import unit
 import numpy as np
 from copy import copy
+import netCDF4
 
 from filters import (
     gaussian_filter_2d, gaussian_filter_3d,
     sobel_filter_2d, sobel_filter_3d)
 
+from pyparsing import Word, alphas, nums, Group, Suppress, Combine, tokenMap
+from datetime import date, timedelta
+
 R_earth = 6.371e3 * unit.km
 day = 0.0027378507871321013 * unit.year
+
+
+def parse_time_units(units):
+    p_int = Word(nums).setParseAction(tokenMap(int))
+    p_date = Group(p_int('year') + Suppress('-') +
+                   p_int('month') + Suppress('-') +
+                   p_int('day'))('date').setParseAction(
+                       tokenMap(lambda args: date(*args)))
+    p_time_unit = Word(alphas)('units') + Suppress("since") + p_date
+    result = p_time_unit.parseString(units)
+    return result['units'], result['date'][0]
 
 
 def is_linear(a, eps=1e-3):
@@ -15,21 +30,108 @@ def is_linear(a, eps=1e-3):
     return x < eps
 
 
+def overlap_idx(t1, t2):
+    idx = np.where(t1 >= t2[0])[0]
+    if len(idx) == 0:
+        return slice(None)
+    else:
+        return slice(0, idx[0])
+
+
+class File(object):
+    def __init__(self, f):
+        self.path = f
+        self.data = netCDF4.Dataset(f, 'r', format='NETCDF4')
+        self.bounds = slice(None)
+
+    @property
+    def time(self):
+        return self.data.variables['time'][self.bounds]
+
+    @property
+    def lat(self):
+        return self.data.variables['lat'][:]
+
+    @property
+    def lon(self):
+        return self.data.variables['lon'][:]
+
+    def get(self, var):
+        return self.data.variables[var][self.bounds]
+
+    @property
+    def time_units(self):
+        dt, t0 = parse_time_units(self.data.variables['time'].units)
+        return dt, t0
+
+
+class DataSet(object):
+    """Deals with sets of NetCDF files, combines data, and
+    generates a valid :py:class:`Box` instance from these files.
+    """
+    pattern = "{variable}_*mon_{model}_{scenario}" \
+              "_{realization}_??????-??????.nc"
+
+    def __init__(self, *, path, model, variable, scenario, realization):
+        self.path = path
+        self.model = model
+        self.variable = variable
+        self.scenario = scenario
+        self.realization = realization
+
+        self._box = None
+
+    def glob(self):
+        return list(self.path.glob(self.pattern.format(**self.__dict__)))
+
+    def load(self):
+        self.files = sorted(
+            list(map(File, self.glob())),
+            key=lambda f: f.time[0])
+
+        bounds = [overlap_idx(self.files[i].time, self.files[i+1].time)
+                  for i in range(len(self.files) - 1)] + [slice(None)]
+
+        for f, b in zip(self.files, bounds):
+            f.bounds = b
+
+    @property
+    def box(self):
+        if self._box is None:
+            time = np.concatenate([f.time for f in self.files])
+            lat = self.files[0].lat
+            lon = self.files[0].lon
+            dt, t0 = self.files[0].time_units
+            self._box = Box(time, lat, lon, dt, t0)
+
+        return self._box
+
+    @property
+    def data(self):
+        return np.concatenate([f.get(self.variable) for f in self.files])
+
+
 class Box:
     """Stores properties of the coordinate system used."""
-    def __init__(self, time, lat, lon):
+    def __init__(self, time, lat, lon,
+                 time_units='days',
+                 time_start=date(1850, 1, 1)):
         self.time = time
         self.lat = lat
         self.lon = lon
 
+        self.time_units = time_units
+        self.time_start = time_start
+
     @staticmethod
     def from_netcdf(nc):
-        """Obtain latitude, longitude and time axes from a given NetCDF
-        object."""
+        """Obtain latitude, longitude and time axes from a given
+        NetCDF object."""
         lat = nc.variables['lat'][:]
         lon = nc.variables['lon'][:]
         time = nc.variables['time'][:]
-        return Box(time, lat, lon)
+        dt, t0 = parse_time_units(nc.variables['time'].units)
+        return Box(time, lat, lon, dt, t0)
 
     @staticmethod
     def generate(n_lon):
@@ -41,6 +143,19 @@ class Box:
         lon = np.linspace(0., 360., n_lon, endpoint=False)
         time = None
         return Box(time, lat, lon)
+
+    def date(self, value):
+        try:
+            return (self.date(t) for t in value)
+        except TypeError:
+            pass
+
+        kwargs = {self.time_units: value}
+        return self.time_start + timedelta(**kwargs)
+
+    @property
+    def dates(self):
+        return list(self.date(self.time))
 
     def __getitem__(self, s):
         """Slices the box in the same way as you would slice data."""
